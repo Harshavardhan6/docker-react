38cae42e6adb1dd87eb8df3527a0daaf1549cd79
Harsha Pilli <Pilli@ars.de>
2022-11-05 12:12:33 +0100

    changed the message

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          katravelli
+          em ro
         </p>
         <a
           className="App-link"
